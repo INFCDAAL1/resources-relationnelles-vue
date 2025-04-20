227dc5463bc05d6a81172c2686fa33182481802a
Q300Z <Q300Zhomas@gmail.com>
2025-04-21 00:02:04 +0200

    chore(App): Le theme sombre est maintenant synchro avec le theme système

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
+<script lang="ts" setup>
+import {useTheme} from "vuetify/framework";
+
+const theme = useTheme()
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+const updateTheme = () => {
+  theme.global.name.value = darkThemeMq.matches ? "dark" : "light";
+};
+
+onMounted(() => {
+  darkThemeMq.addEventListener("change", updateTheme);
+});
+</script>
+
 <template>
   <v-app>
     <router-view />
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  //
-</script>
