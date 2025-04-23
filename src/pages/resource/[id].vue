796471b48026d6fe2a9984e2efb6413fd40df0a0
Q300Z <Q300Zhomas@gmail.com>
2025-04-23 17:48:13 +0200

    feat(resource): Amélioration la page détail ressource

--- src/pages/resource/[id].vue
+++ src/pages/resource/[id].vue
@@ -19,44 +19,61 @@ const storeComment = useCommentStore()
 const item: Ref<Resource | undefined> = ref();
 const comments: Ref<Comment[]> = ref([]);
 
-onMounted(() => {
-  const { id } = route.params as RouteParams
-  item.value = store.getResourceById(Number(id))
-  comments.value = storeComment.getApprovedCommentsByResourceId(Number(id)) || []
+onMounted(async () => {
+  const { id } = route.params as RouteParams;
+  const resourceId = Number(id);
+  item.value = store.getResourceById(resourceId);
+
   if (!item.value) {
-    console.error(`Resource with id ${id} not found`);
-    router.back()
-    return;
+    try {
+      const res = await axios.get(`resources/${resourceId}`);
+      item.value = res.data.data;
+    } catch (err) {
+      console.error(`Resource with id ${id} not found`, err);
+      router.back();
+      return;
+    }
   }
 
-  if(comments.value){
-    axios.get(`comments?resource_id=${item.value.id}`).then((res) => {
-      if (res.status === 200) {
-        storeComment.setComments(res.data.data)
-        comments.value = res.data.data
-      }
-    }).catch((error) => {
-      console.error('Error fetching comments:', error);
-    })
+  try {
+    const res = await axios.get(`comments?resource_id=${resourceId}`);
+    if (res.status === 200) {
+      storeComment.setComments(res.data.data);
+      comments.value = res.data.data;
+    }
+  } catch (error) {
+    console.error("Error fetching comments:", error);
   }
+});
+
+const getDownloadLink = computed(() => {
+  if (item.value&& item.value.download_url)
+    return item.value.download_url
+  else return ''
 })
 
 </script>
 
 <template>
   <div class="d-flex flex-column ga-5">
-    <v-card v-if="!item" title="Resource loading..."></v-card>
-    <div v-else>
+    <v-card v-if="!item" title="Chargement de la ressource..."></v-card>
+    <v-card v-else>
       <ResourceCard :item="item" @toggle-favorite="store.toggleResourceFavorite">
         <template #action>
-          <v-btn variant="tonal" append-icon="mdi-arrow-down">Télécharger</v-btn>
+          <v-btn
+            variant="tonal"
+            append-icon="mdi-arrow-down"
+            :disabled="!getDownloadLink"
+            :href="getDownloadLink"
+          >
+            Télécharger
+          </v-btn>
         </template>
       </ResourceCard>
-    </div>
-    <v-card v-if="!comments" title="Comments loading..."></v-card>
-    <div v-else>
-      <CommentList :items="comments" no-filter/>
-    </div>
+    </v-card>
+
+    <v-card v-if="!comments.length" title="Aucun commentaire pour cette ressource."></v-card>
+    <CommentList v-else :items="comments" no-filter />
   </div>
 
 </template>
